d359eb8ca2ee360a1a4c09af859e24b787655931
holmis <alesholman@gmail.com>
2025-08-27 00:17:46 +0200

    still fixing build, common I want to go to sleep

--- src/routes/62-three-js-checkerboard-text/app.js
+++ src/routes/62-three-js-checkerboard-text/app.js
@@ -9,7 +9,7 @@ import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 import textVertexShader from './shaders/textVertex.glsl';
 import textFragmentShader from './shaders/textFragment.glsl';
-import { setupCameraPane } from '$lib/utils/tweakpane/utils.js';
+import { setupCameraPane } from '$lib/utils/tweakpane/utils';
 import { Pane } from 'tweakpane';
 import { on } from 'svelte/events';
 
